f7bf356bc1f0609b02640849d71746083d602c8a
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2024-09-16 16:01:19 +0600

    Tabbed task schedule in active and tag pages

--- src/renderer/widgets/TasksBySchedule.tsx
+++ src/renderer/widgets/TasksBySchedule.tsx
@@ -1,5 +1,6 @@
-import { List, Typography, Divider } from '@mui/material';
-import { useEffect, Fragment } from 'react';
+import { useEffect, SyntheticEvent, Fragment, useState } from 'react';
+import { List, Typography, Divider, Tab, Box } from '@mui/material';
+import { TabContext, TabList, TabPanel } from '@mui/lab';
 import {
   TaskScheduleTypeEnum,
   DaysInAWeek,
@@ -71,144 +72,174 @@ function TasksBySchedule({
     }
   }, [setNotifier, taskRescheduleError]);
 
+  const [tabValue, setTabValue] = useState<TaskScheduleTypeEnum>(
+    TaskScheduleTypeEnum.Unscheduled,
+  );
+  const handleTabChange = (
+    event: SyntheticEvent,
+    newValue: TaskScheduleTypeEnum,
+  ) => {
+    setTabValue(newValue);
+  };
+
   return (
     <div>
-      <Typography mt={2} mb={1} variant="h6">
-        {TaskScheduleTypeEnum.Unscheduled}
-      </Typography>
-      {unscheduledTasks.length === 0 && (
-        <Typography variant="body2">No tasks unscheduled</Typography>
-      )}
-      {unscheduledTasks.length > 0 && (
-        <List>
-          {unscheduledTasks.map((task) => {
-            return (
-              <Fragment key={task.id}>
-                <TodoListItem
-                  schedule={task.schedule as TaskScheduleTypeEnum}
-                  taskTitle={task.title}
-                  tags={task.tags}
-                  isCompleted={
-                    task.completionStatus === TaskCompletionStatusEnum.COMPLETE
-                  }
-                  onChange={(e) =>
-                    onToggleTaskCompletionStatus(
-                      task.id,
-                      e.target.checked,
-                      undefined,
-                    )
-                  }
-                  onFail={() => onTaskFailure(task.id)}
-                  onReschedule={(rescheduledTime) =>
-                    onTaskReschedule(task.id, rescheduledTime)
-                  }
-                  onTaskEdit={() => handleTaskEdit(task.id)}
-                  showClock
-                  key={task.id}
-                />
-                <Divider />
-              </Fragment>
-            );
-          })}
-        </List>
-      )}
+      <TabContext value={tabValue}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleTabChange} aria-label="lab API tabs example">
+            <Tab
+              label={TaskScheduleTypeEnum.Unscheduled}
+              value={TaskScheduleTypeEnum.Unscheduled}
+            />
+            <Tab
+              label={TaskScheduleTypeEnum.Once}
+              value={TaskScheduleTypeEnum.Once}
+            />
+            <Tab
+              label={TaskScheduleTypeEnum.Daily}
+              value={TaskScheduleTypeEnum.Daily}
+            />
+            <Tab
+              label={TaskScheduleTypeEnum.SpecificDaysInAWeek}
+              value={TaskScheduleTypeEnum.SpecificDaysInAWeek}
+            />
+          </TabList>
+        </Box>
 
-      <Typography mt={5} mb={1} variant="h6">
-        {TaskScheduleTypeEnum.Once}
-      </Typography>
-      {oneOffTasks.length === 0 && (
-        <Typography variant="body2">No one-off tasks scheduled.</Typography>
-      )}
-      {oneOffTasks.length > 0 && (
-        <List>
-          {oneOffTasks.map((task) => {
-            return (
-              <Fragment key={task.id}>
-                <TodoListItem
-                  schedule={task.schedule as TaskScheduleTypeEnum}
-                  taskTitle={task.title}
-                  tags={task.tags}
-                  isCompleted={
-                    task.completionStatus === TaskCompletionStatusEnum.COMPLETE
-                  }
-                  onChange={(e) =>
-                    onToggleTaskCompletionStatus(
-                      task.id,
-                      e.target.checked,
-                      undefined,
-                    )
-                  }
-                  onFail={() => onTaskFailure(task.id)}
-                  onReschedule={(rescheduledTime) =>
-                    onTaskReschedule(task.id, rescheduledTime)
-                  }
-                  onTaskEdit={() => handleTaskEdit(task.id)}
-                  showClock
-                  key={task.id}
-                  dueDateLabel={task.dueDate}
-                />
-                <Divider />
-              </Fragment>
-            );
-          })}
-        </List>
-      )}
+        <TabPanel value={TaskScheduleTypeEnum.Unscheduled}>
+          {unscheduledTasks.length === 0 && (
+            <Typography variant="body2">No tasks unscheduled</Typography>
+          )}
+          {unscheduledTasks.length > 0 && (
+            <List>
+              {unscheduledTasks.map((task) => {
+                return (
+                  <Fragment key={task.id}>
+                    <TodoListItem
+                      schedule={task.schedule as TaskScheduleTypeEnum}
+                      taskTitle={task.title}
+                      tags={task.tags}
+                      isCompleted={
+                        task.completionStatus ===
+                        TaskCompletionStatusEnum.COMPLETE
+                      }
+                      onChange={(e) =>
+                        onToggleTaskCompletionStatus(
+                          task.id,
+                          e.target.checked,
+                          undefined,
+                        )
+                      }
+                      onFail={() => onTaskFailure(task.id)}
+                      onReschedule={(rescheduledTime) =>
+                        onTaskReschedule(task.id, rescheduledTime)
+                      }
+                      onTaskEdit={() => handleTaskEdit(task.id)}
+                      showClock
+                      key={task.id}
+                    />
+                    <Divider />
+                  </Fragment>
+                );
+              })}
+            </List>
+          )}
+        </TabPanel>
+        <TabPanel value={TaskScheduleTypeEnum.Once}>
+          {oneOffTasks.length === 0 && (
+            <Typography variant="body2">No one-off tasks scheduled.</Typography>
+          )}
+          {oneOffTasks.length > 0 && (
+            <List>
+              {oneOffTasks.map((task) => {
+                return (
+                  <Fragment key={task.id}>
+                    <TodoListItem
+                      schedule={task.schedule as TaskScheduleTypeEnum}
+                      taskTitle={task.title}
+                      tags={task.tags}
+                      isCompleted={
+                        task.completionStatus ===
+                        TaskCompletionStatusEnum.COMPLETE
+                      }
+                      onChange={(e) =>
+                        onToggleTaskCompletionStatus(
+                          task.id,
+                          e.target.checked,
+                          undefined,
+                        )
+                      }
+                      onFail={() => onTaskFailure(task.id)}
+                      onReschedule={(rescheduledTime) =>
+                        onTaskReschedule(task.id, rescheduledTime)
+                      }
+                      onTaskEdit={() => handleTaskEdit(task.id)}
+                      showClock
+                      key={task.id}
+                      dueDateLabel={task.dueDate}
+                    />
+                    <Divider />
+                  </Fragment>
+                );
+              })}
+            </List>
+          )}
+        </TabPanel>
+        <TabPanel value={TaskScheduleTypeEnum.Daily}>
+          {dailyTasks.length === 0 && (
+            <Typography variant="body2">No daily tasks.</Typography>
+          )}
+          {dailyTasks.length > 0 && (
+            <List>
+              {dailyTasks.map((task) => {
+                return (
+                  <Fragment key={task.id}>
+                    <TodoListItem
+                      schedule={task.schedule as TaskScheduleTypeEnum}
+                      taskTitle={task.title}
+                      tags={task.tags}
+                      onChange={() => {}}
+                      key={task.id}
+                      onTaskEdit={() => handleRepetitiveTaskEdit(task.id)}
+                    />
+                    <Divider />
+                  </Fragment>
+                );
+              })}
+            </List>
+          )}
+        </TabPanel>
+        <TabPanel value={TaskScheduleTypeEnum.SpecificDaysInAWeek}>
+          {specificDaysInAWeekTasks.length === 0 && (
+            <Typography variant="body2">No tasks.</Typography>
+          )}
+          {specificDaysInAWeekTasks.length > 0 && (
+            <List>
+              {specificDaysInAWeekTasks.map((task) => {
+                const days = Object.values(DaysInAWeek).filter(
+                  (day) => task[day],
+                );
 
-      <Typography mt={5} mb={1} variant="h6">
-        {TaskScheduleTypeEnum.Daily}
-      </Typography>
-      {dailyTasks.length === 0 && (
-        <Typography variant="body2">No daily tasks.</Typography>
-      )}
-      {dailyTasks.length > 0 && (
-        <List>
-          {dailyTasks.map((task) => {
-            return (
-              <Fragment key={task.id}>
-                <TodoListItem
-                  schedule={task.schedule as TaskScheduleTypeEnum}
-                  taskTitle={task.title}
-                  tags={task.tags}
-                  onChange={() => {}}
-                  key={task.id}
-                  onTaskEdit={() => handleRepetitiveTaskEdit(task.id)}
-                />
-                <Divider />
-              </Fragment>
-            );
-          })}
-        </List>
-      )}
-
-      <Typography mt={5} mb={1} variant="h6">
-        {TaskScheduleTypeEnum.SpecificDaysInAWeek}
-      </Typography>
-      {specificDaysInAWeekTasks.length === 0 && (
-        <Typography variant="body2">No tasks.</Typography>
-      )}
-      {specificDaysInAWeekTasks.length > 0 && (
-        <List>
-          {specificDaysInAWeekTasks.map((task) => {
-            const days = Object.values(DaysInAWeek).filter((day) => task[day]);
-
-            return (
-              <Fragment key={task.id}>
-                <TodoListItem
-                  schedule={task.schedule as TaskScheduleTypeEnum}
-                  taskTitle={task.title}
-                  tags={task.tags}
-                  onChange={() => {}}
-                  onTaskEdit={() => handleRepetitiveTaskEdit(task.id)}
-                  showClock
-                  key={task.id}
-                  dayLabels={days}
-                />
-                <Divider />
-              </Fragment>
-            );
-          })}
-        </List>
-      )}
+                return (
+                  <Fragment key={task.id}>
+                    <TodoListItem
+                      schedule={task.schedule as TaskScheduleTypeEnum}
+                      taskTitle={task.title}
+                      tags={task.tags}
+                      onChange={() => {}}
+                      onTaskEdit={() => handleRepetitiveTaskEdit(task.id)}
+                      showClock
+                      key={task.id}
+                      dayLabels={days}
+                    />
+                    <Divider />
+                  </Fragment>
+                );
+              })}
+            </List>
+          )}
+        </TabPanel>
+      </TabContext>
     </div>
   );
 }
