6e8900c1a8ec8e91443afd413b6a043afe563656
ArnobChowdhury <chowdhury.sami.al@gmail.com>
2025-11-06 15:34:03 +0600

    refator(alltasks): get rid of unused widget

--- src/renderer/widgets/index.tsx
+++ src/renderer/widgets/index.tsx
@@ -1,8 +1,7 @@
 import AddTask from './AddTask';
 import HabitTracker from './Habit';
 import TodoList from './TodoList';
-import AllTasks from './AllTasks';
 import EditTask from './EditTask';
 import TasksBySchedule from './TasksBySchedule';
 
-export { AddTask, HabitTracker, TodoList, AllTasks, EditTask, TasksBySchedule };
+export { AddTask, HabitTracker, TodoList, EditTask, TasksBySchedule };
